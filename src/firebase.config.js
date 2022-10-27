933a91ae2b43987a02cd66ed71d230cbadd9ba7c
sbswag9@gmail.com <sbswag9@gmail.com>
2022-10-27 17:04:45 +0600

    added firebase config

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5misgafif0t7bSy_uhGI77SEP8aMYt7o",
+  authDomain: "learnera-d16c5.firebaseapp.com",
+  projectId: "learnera-d16c5",
+  storageBucket: "learnera-d16c5.appspot.com",
+  messagingSenderId: "925201331660",
+  appId: "1:925201331660:web:dbad9f89552f0f6c4e5969",
+  measurementId: "G-1ZQ5WPEGM0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
