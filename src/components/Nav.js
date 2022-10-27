bced7e38f00847ca50121c105e65bfb23063fca9
sbswag9@gmail.com <sbswag9@gmail.com>
2022-10-27 19:45:08 +0600

    added logout function and conditional nav rendering

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useContext } from "react";
 import { FaRegMoon } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthProvider";
 
 function Nav() {
+  const { user, logOut } = useContext(AuthContext);
+
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {})
+      .catch((error) => console.error(error));
+  };
+
   return (
     <header className="flex justify-between py-14 items-center max-h-48 mb-14">
       <h1 className="text-3xl font-bold text-myblue">Learnera.</h1>
@@ -27,10 +38,31 @@ function Nav() {
       </div>
       <div className="flex items-center">
         <FaRegMoon className="cursor-pointer" />
-        <button className="mx-5 rounded-md bg-myblue px-2 py-1 text-white">
-          Login
-        </button>
-        <img src="" alt="" />
+        {user?.uid ? (
+          <>
+            <Link to="/profile">
+              <img
+                src={user?.photoURL}
+                alt=""
+                className="w-[50px] h-[50px] rounded-full ml-5 cursor-pointer"
+              />
+            </Link>
+            <button
+              className="mx-5 rounded-md bg-myblue px-2 py-1 text-white"
+              onClick={handleLogOut}
+            >
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">
+              <button className="mx-5 rounded-md bg-myblue px-2 py-1 text-white">
+                Login
+              </button>
+            </Link>
+          </>
+        )}
       </div>
     </header>
   );
